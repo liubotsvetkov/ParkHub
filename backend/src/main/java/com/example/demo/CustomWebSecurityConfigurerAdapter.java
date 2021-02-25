913b006efad0636b6dcbdd5fe520458afe51e2de
DESKTOP-5E01UN5\Lyubo <liubotsvetkov@gmail.com>
2021-02-25 18:22:36 +0200

    Added Security config

--- backend/src/main/java/com/example/demo/CustomWebSecurityConfigurerAdapter.java
+++ backend/src/main/java/com/example/demo/CustomWebSecurityConfigurerAdapter.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +26,7 @@ public class CustomWebSecurityConfigurerAdapter extends
                 .and()
                 .httpBasic();
         http
-                .cors();
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
 
     @Autowired

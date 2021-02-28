ac49a9a0aeeea525747aeec0ebe9844c22e654a9
Fjolla Dedaj <dedajfjolla@gmail.com>
2021-02-28 12:26:07 +0100

    Add get request for parking spaces

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -19,44 +19,63 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
+      dataFetched: null,
       markers: [
         {
           position: {
             lat: 1.38,
             lng: 103.8
           },
-          free: true,
+          free: true
         },
         {
           position: {
             lat: 53.168057,
             lng: 8.654234
           },
-          free: true,
+          free: true
         },
         {
           position: {
             lat: 42.659372,
             lng: 23.314266
           },
-          free: false,
+          free: false
         }
       ]
     };
   },
   methods: {
     markerColor(isFree) {
-      if(isFree) {
-        return 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+      if (isFree) {
+        return "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
       } else {
-        return 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+        return "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
       }
+    },
+    fetchData() {
+      axios({
+        method: "get",
+        url: "http://localhost:8089/viewAllParkingSlots",
+        headers: {
+          "Access-Control-Allow-Origin": true,
+          Authorization: "Basic ZmpvbGxhOjEyMw=="
+        }
+      })
+        .then(({ data }) => {
+          this.dataFetched = data;
+          console.log("res", this.dataFetched);
+        })
+        .catch(err => console.log(err));
     }
   },
   mounted() {
+    this.fetchData();
     this.$refs.mapRef.$mapPromise.then(map => {
       map.panTo({ lat: 1.38, lng: 103.8 });
     });
